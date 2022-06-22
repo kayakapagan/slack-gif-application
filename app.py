c1eaf9c8035c2844d67d1d0975f5e3b4af04c81e
Kaya Kapagan <kayakapagan@sabanciuniv.edu>
2022-06-22 10:58:46 +0200

    Refactor: Some code parts refactored and unnecessary functions are deleted.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import logging
 import os
+import logging
 import requests
 
-from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+from dotenv import load_dotenv
 load_dotenv()
 
 SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
@@ -13,50 +13,9 @@ SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 GIPHY_KEY = os.environ["GIPHY_KEY"]
 
-
-
-app = App(token=SLACK_BOT_TOKEN, signing_secret=SLACK_SIGNING_SECRET, name="Joke Bot")
+app = App(token=SLACK_BOT_TOKEN, signing_secret=SLACK_SIGNING_SECRET, name="Gif Finder")
 logger = logging.getLogger(__name__)
 
-
-
-
-
-@app.command("/greet")
-def message_greet(ack, say, command):
-    ack()
-    say(f"Hey {command['text']}")
-
-# To learn available listener arguments,
-# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
-@app.message("hello")
-def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
-    say(f"Hey there <@{message['user']}>!")
-
-@app.message("hey")
-def message_hey(message, say):
-    # say() sends a message to the channel where the event was triggered
-    say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
-                "accessory": {
-                    "type": "button",
-                    "text": {"type": "plain_text", "text": "Click Me"},
-                    "action_id": "button_click"
-                }
-            }
-        ],
-        text=f"Hey there <@{message['user']}>!"
-    )
-
-@app.action("button_click")
-def action_button_click(body, ack, say):
-    # Acknowledge the action
-    ack()
-    say(f"<@{body['user']['id']}> clicked the button")
 
 @app.action("channel_selected")
 def action_channel_selected(body, ack, say):
@@ -97,19 +56,21 @@ def action_item_selected(body, ack, say):
     ]
     
     say(blocks=blocks)
-    
 
-@app.command("/find-gif")
-def find_gif(ack, say, command):
-    ack()
+def generate_section_selecting_gif_options(data):
+    generated_options = []
+    for i in range(len(data)):
+        generated_options.append({
+            "text": {
+                "type": "plain_text",
+                "text": str(i+1)
+            },
+            "value": data[i]["images"]["original"]["url"]
+        })
+    return generated_options
 
-    res = requests.get(f"https://api.giphy.com/v1/gifs/search?api_key={GIPHY_KEY}&limit=10&q={command['text']}")
-
-    data = res.json()["data"]
-    url = data[0]["images"]["original"]["url"]
-    print(url)
+def generate_find_gif_blocks(command, data):
     generated_blocks = []
-
     for i in range(len(data)):
         generated_blocks.append({
             "type": "image",
@@ -121,6 +82,8 @@ def find_gif(ack, say, command):
             "alt_text": f"Choice {i+1} from Giphy search with {command['text']}",
             "image_url": data[i]["images"]["original"]["url"],
         })
+    
+    generated_options = generate_section_selecting_gif_options(data)
 
     generated_blocks.append({
         "type": "section",
@@ -136,106 +99,21 @@ def find_gif(ack, say, command):
                 "type": "plain_text",
                 "text": "Select a gif number"
             },
-            "options": [
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "1"
-                        },
-                        "value": data[0]["images"]["original"]["url"]
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "2"
-                        },
-                        "value": data[1]["images"]["original"]["url"]
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "3"
-                        },
-                        "value": data[2]["images"]["original"]["url"]
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "4"
-                        },
-                        "value": data[3]["images"]["original"]["url"]
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "5"
-                        },
-                        "value": data[4]["images"]["original"]["url"]
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "6"
-                        },
-                        "value": data[5]["images"]["original"]["url"]
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "7"
-                        },
-                        "value": data[6]["images"]["original"]["url"]
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "8"
-                        },
-                        "value": data[7]["images"]["original"]["url"]
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "9"
-                        },
-                        "value": data[8]["images"]["original"]["url"]
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "10"
-                        },
-                        "value": data[9]["images"]["original"]["url"]
-                    }
-            ]
+            "options": generated_options
         }
     })
 
-    # blocks = [
-    #     # {
-	# 	# 	"type": "section",
-	# 	# 	"block_id": "section567",
-	# 	# 	"text": {
-	# 	# 		"type": "mrkdwn",
-	# 	# 		"text": "This is a section block with an accessory image."
-	# 	# 	},
-	# 	# 	"accessory": {
-	# 	# 		"type": "image",
-	# 	# 		"image_url": url,
-	# 	# 		"alt_text": "cute cat"
-	# 	# 	}
-	# 	# },
-    #     {
-    #         "type": "image",
-    #         "title": {
-    #             "type": "plain_text",
-    #             "text": "Please enjoy this photo of a kitten"
-    #         },
-    #         "block_id": "image4",
-    #         "image_url": url,
-    #         "alt_text": "An incredibly cute kitten."
-    #     }
-    # ]
+    return generated_blocks
+
+
+@app.command("/find-gif")
+def find_gif(ack, say, command):
+    ack()
+
+    res = requests.get(f"https://api.giphy.com/v1/gifs/search?api_key={GIPHY_KEY}&limit=10&q={command['text']}")
+    data = res.json()["data"]
+
+    generated_blocks = generate_find_gif_blocks(command, data)
 
     say(blocks=generated_blocks)
 
